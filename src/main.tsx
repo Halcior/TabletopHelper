2479209ad206a992231de087abfab5948d6036fb
Halcior <131879934+Halcior@users.noreply.github.com>
2026-09-01 17:38:21 +0200

    Load simplified battle UI overrides

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './styles.css'
 import './lifecycle.css'
 import './contextPolish.css'
 import './sharedSessions.css'
+import './uiSimplify.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
