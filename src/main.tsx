876d1f1d6150853b27a56630f4870ac20db4ace0
Halcior <131879934+Halcior@users.noreply.github.com>
2026-09-01 17:41:22 +0200

    Load focused shared room UI

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './lifecycle.css'
 import './contextPolish.css'
 import './sharedSessions.css'
 import './uiSimplify.css'
+import './uiFocus.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
