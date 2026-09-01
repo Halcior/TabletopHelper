5aee8a32d0f1dc887d8a7f96e83af17810678b50
Halcior <131879934+Halcior@users.noreply.github.com>
2026-09-01 12:59:42 +0200

    Apply context dashboard polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './lifecycle.css'
+import './contextPolish.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
