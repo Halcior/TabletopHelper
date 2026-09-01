e6d3f1a498c8088da2fb2e6ac32f7b27b2bec013
Halcior <131879934+Halcior@users.noreply.github.com>
2026-09-01 14:19:32 +0200

    Load shared session styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App'
 import './styles.css'
 import './lifecycle.css'
 import './contextPolish.css'
+import './sharedSessions.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
